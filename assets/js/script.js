ead3d81fbd67a05521d9d96d640422b470cc0168
lorennadev <lorenaneves.dev@icloud.com>
2024-05-11 14:37:22 -0300

    ajustes no design

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const teachers = document.querySelector('#teachers'); 
+const card1 = document.querySelector('.card1');
+const card2 = document.querySelector('.card2');
+const card3 = document.querySelector('.card3');
+const card4 = document.querySelector('.card4');
+const card5 = document.querySelector('.card5');
+
+
+
