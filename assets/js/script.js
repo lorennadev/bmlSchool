3ae961470d21ce48fa166ad3c15ee6be7ae10525
lorennanvs <lorenaneves.dev@icloud.com>
2024-08-19 19:57:04 -0300

    Fechamento do novo desing e marcação de breakpoints

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
-const teachers = document.querySelector('#teachers'); 
-const card1 = document.querySelector('.card1');
-const card2 = document.querySelector('.card2');
-const card3 = document.querySelector('.card3');
-const card4 = document.querySelector('.card4');
-const card5 = document.querySelector('.card5');
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelector('.slides');
+    const slideCount = document.querySelectorAll('.slide').length;
+    let index = 0;
 
+    function showSlide(i) {
+        if (i >= slideCount) {
+            index = 0;
+        } else if (i < 0) {
+            index = slideCount - 1;
+        } else {
+            index = i;
+        }
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
 
+    document.querySelector('.prev').addEventListener('click', function() {
+        showSlide(index - 1);
+    });
 
+    document.querySelector('.next').addEventListener('click', function() {
+        showSlide(index + 1);
+    });
+
+    // Opcional: Auto play
+    setInterval(function() {
+        showSlide(index + 1);
+    }, 15000);
+});
