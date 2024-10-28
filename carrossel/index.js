3f30e13ed639d38828fd4d5bcad647a95ecb5e16
lorennadev <lorenaneves.dev@icloud.com>
2024-10-28 16:02:33 -0300

    Criação do carrossel

---
+++ carrossel/index.js
@@ -0,0 +1,20 @@
+const box = document.querySelector('.container');
+const imagens = document.querySelectorAll('.container img');
+
+let contador = 0;
+
+function slider() {
+    contador++;
+
+    if (contador > imagens.length - 1) {
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 700}px)`
+}
+
+imagens.addEventListener('click', () => {
+    contador++
+});
+
+setInterval(slider, 5000)
